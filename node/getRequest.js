707a73d1e4b2d937b5c6d7b377a11ad87d558a3e
AlexKlimenkov <shurick.klimenkov@gmail.com>
2018-12-21 18:20:02 +0300

    [fix] node/getRequest.js - switch back to http module

--- node/getRequest.js
+++ node/getRequest.js
@@ -1,29 +1,30 @@
-const request = require('request');
+const http = require('http');
 const company = "YOUR_TEAMWORK_SITE_NAME";
 const key = "YOUR_API_KEY";
 
-const base64 = Buffer.from(key + ":xxx").toString("base64");
+const base64 = Buffer.from(`${key}:xxx`).toString("base64");
 
 const options = {
-    uri: `https://${company}.teamwork.com/projects.json`,
+    hostname: `${company}.teamwork.com`,
+    path: "/projects.json",
     method: "GET",
     headers: {
         "Authorization": `BASIC ${base64}`,
         "Content-Type": "application/json"
     }
 };
- 
-const req = request(options, (error, res, body) => {
-    if (error) {
-        console.log(`ERROR: ${error.message}`);
-        return;
-    }
 
+const req = http.request(options, function(res) {
     console.log(`STATUS: ${res.statusCode}`);
     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
     res.setEncoding("utf8");
+    res.on("data", function (chunk) {
+        console.log(`BODY: ${chunk}`);
+    });
+});
 
-    console.log(`BODY: ${body}`);
+req.on("error", function(e) {
+    console.log(`ERROR: ${error.message}`);
 });
 
 req.end();
