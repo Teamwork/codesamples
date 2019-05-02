71ba56f1d73984c10ef5e17966def251b680b3d5
robpeters-appliednt <38111647+robpeters-appliednt@users.noreply.github.com>
2019-05-02 10:59:49 -0600

    For newer versions of urllib3
    https://urllib3.readthedocs.io/en/latest/user-guide.html#certificate-verification

--- python3/getRequest.py
+++ python3/getRequest.py
@@ -1,6 +1,7 @@
 import urllib3
+import certifi
 
-http = urllib3.PoolManager()
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
 company = "YOUR_TEAMWORK_SITE_NAME"
 key = "YOUR_API_KEY"
